ce963d0f39e29933b19741bc3fcd36d5f73585a7
abhishek-shah-vst-au4 <54413214+abhishek-shah-vst-au4@users.noreply.github.com>
2021-02-05 22:04:26 +0530

    have to do correction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRender: true //This is where I am having problems
+      
+    };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const style = {
+      width:'100px'
+    }
+    const style2 = {
+      width:'200px'
+    }
+
+ 
+  }
+
+
+
+  render() {
+    const isRender = this.state.isRender;
+    const style = this.style;
+    const style2 = this.style2;
+
+    return (
+      <div>
+        {isRender ? (
+          <div>
+           <img src="https://cdn.quicksell.co/-KpFi1XEDgH1mY5k1fSo/products_100/-M7q3olCZCehB9AGbn2y.jpg" style={style}></img>
+          </div>
+        ) : (
+          <div>
+             <img src="https://cdn.quicksell.co/-KpFi1XEDgH1mY5k1fSo/products_100/-M7q3olCZCehB9AGbn2y.jpg" style2={style2}></img>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
+
 
 export default App;
